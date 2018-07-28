dab068dcd2e2562c0f0876464db1d2a0c42d1d15
prabhu-raja <prabhuraja@gmail.com>
2018-07-28 17:34:55 -0400

    added web merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpackMerge = require("webpack-merge");
 
-module.exports = ({ mode }) => {
-  console.log(mode);
-  return { 
+const modeConfig = (env) => {
+  return require(`./build-utils/webpack.${env}`)(env);
+}
+// const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+module.exports = ({ mode, presets } = {mode: 'production', presets: []}) => {
+  console.log('🐶', mode);
+  console.log('🐒', modeConfig);
+  return webpackMerge({ 
     mode,
     output: {
       filename: "bundle.js"
     },
     plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
-  }
-}
+  }, 
+    modeConfig(mode));
+};
